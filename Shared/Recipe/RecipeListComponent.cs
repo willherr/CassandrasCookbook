6ca5c2da1370fcc08157096a883c94da564e32a2
William Herrmann <herrmanw@mail.gvsu.edu>
2020-09-18 13:59:42 -0400

    add edit fab

--- Shared/Recipe/RecipeListComponent.cs
+++ Shared/Recipe/RecipeListComponent.cs
@@ -22,6 +22,7 @@ namespace CassandrasCookbook.Shared.Recipe
                 _ => AllRecipes
             }).OrderBy(x => x.Title);
         }
+        protected string NewRecipeRoute => $"/recipe/new/{true}/{(int)Type}";
 
         public static IEnumerable<RecipeItem> AllRecipes { get; set; } = new List<RecipeItem>();
         public static IEnumerable<RecipeItem> AppetizerRecipes => AllRecipes.Where(x => x.Type == Type.Appetizer);
